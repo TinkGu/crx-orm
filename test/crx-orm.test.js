14dc6168a79e7b7bad5a7b2e465d2a068292f4e0
tinkgu <tlwrite@126.com>
2018-02-22 23:50:30 +0800

    [add] v1.0 test

---
+++ test/crx-orm.test.js
@@ -0,0 +1,102 @@
+import orm from '../src'
+import TagModel from './models/Tag'
+
+function createStore(initial) {
+    const adapter = orm.createMemoryAdapter()
+    Object.assign(adapter.snapshot(), initial)
+    return orm.createStore({
+        adapter,
+        schemas: [
+            TagModel,
+        ],
+    })
+}
+
+describe('read model', () => {
+    const tag = {
+        id: 'xxx',
+        name: 'tag-xxx',
+    }
+
+    const store = createStore({
+        'tag:id:xxx': tag,
+        tagIds: [tag.id],
+        'tag:name:tag-xxx': 'xxx',
+    })
+    const tagModel = store.models.tag
+
+    it('should read tag by id', async () => {
+        expect(await tagModel.readTag(tag.id)).toBe(tag)
+    })
+
+    it('should read tag by doc name', async () => {
+        expect(await tagModel.readTag({
+            name: tag.name
+        })).toBe(tag)
+    })
+})
+
+describe('create model', () => {
+    const store = createStore()
+    const tagModel = store.models.tag
+
+    it('should create model、id in ids、indexes', async () => {
+        expect.assertions(3)
+
+        const tag = await tagModel.createTag('china')
+        const tagInRead = await tagModel.readTag(tag.id)
+        const ids = await tagModel.readIds()
+        const nameIndex = await tagModel.readRaw('name', tag.name)
+
+        expect(tagInRead).toBe(tag)
+        expect(ids.includes(tag.id)).toBe(true)
+        expect(nameIndex).toBe(tag.id)
+    })
+})
+
+
+describe('update model', () => {
+    const store = createStore()
+    const tagModel = store.models.tag
+
+    it('should update model and indexes', async () => {
+        expect.assertions(3)
+
+        const tagBefore = await tagModel.createTag('china')
+        const tagAfter = await tagModel.updateTag(tagBefore, x => ({
+            ...x,
+            name: 'zh',
+            desc: 'chinese shorthand',
+        }))
+        const nameIndexBefore = await tagModel.readRaw('name', tagBefore.name)
+        const nameIndexAfter = await tagModel.readRaw('name', tagAfter.name)
+
+        expect(tagAfter).toEqual({
+            id: tagBefore.id,
+            name: 'zh',
+            desc: 'chinese shorthand',
+        })
+
+        expect(nameIndexBefore).toBe(null)
+        expect(nameIndexAfter).toBe(tagBefore.id)
+    })
+})
+
+describe('remove model', () => {
+    const store = createStore()
+    const tagModel = store.models.tag
+
+    it('should remove model、id in ids、indexes', async () => {
+        expect.assertions(3)
+
+        const tag = await tagModel.createTag('china')
+        await tagModel.removeTag(tag.id)
+        const tagAfterRemove = await tagModel.readTag(tag.id)
+        const nameIndex = await tagModel.readRaw('name', tag.name)
+        const ids = await tagModel.readIds()
+
+        expect(tagAfterRemove).toBe(null)
+        expect(ids.includes(tag.id)).toBe(false)
+        expect(nameIndex).toBe(null)
+    })
+})
